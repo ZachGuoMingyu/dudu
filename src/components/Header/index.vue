fcb7799f5950a2874398550b20ab73afa714b6ab
ZachGuoMingyu <139160190@qq.com>
2020-03-13 14:56:06 +0800

    add createComponents

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header">
+  <header id="header">
     <h1>{{title}}</h1>
   </header>
 </template>
@@ -16,25 +16,7 @@ export default {
 };
 </script>
 <style scoped>
-.header {
-  width: 100%;
-  height: 50px;
-  color: #fff;
-  background: #e54847;
-  border-bottom: 1px solid #e54847;
-  position: relative;
-}
-.header h1 {
-  font-size: 18px;
-  text-align: center;
-  line-height: 50px;
-  font-weight: normal;
-}
-.header i {
-  position: absolute;
-  left: 5px;
-  top: 50%;
-  margin-top: -13px;
-  font-size: 26px;
-}
+#header{width:100%; height:50px; color: #fff; background: #e54847; border-bottom: 1px solid #e54847; position: relative;}
+#header h1{ font-size: 18px; text-align: center; line-height: 50px; font-weight: normal; }
+#header i{ position: absolute; left: 5px; top: 50%; margin-top: -13px; font-size: 26px;}
 </style>
--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="main">
     <Header title="我的嘟嘟" />
+      <div id="content">
+        <Login />
+      </div>
     <TabBar />
   </div>
 </template>
@@ -9,12 +12,14 @@
 
 import Header from '@/components/Header'
 import TabBar from '@/components/TabBar'
+import Login from '@/components/Login'
 
 export default {
   name:'Mine',
   components:{
     Header,
-    TabBar
+    TabBar,
+    Login
   },
   data() {
     return {
